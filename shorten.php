aabefbdb1fa584ec98a497670e3e95b0b0bcc2b6
romanianindenmark <mortasipu.razvan@gmail.com>
2021-02-15 11:15:14 +0100

    added the resolve url code

--- shorten.php
+++ shorten.php
@@ -8,7 +8,31 @@ $db = new mysqli("localhost","root","root","urls");
 //get the value from the input having the name "link"
 $url = $_POST['link'];
 
-if($url) {
+//function who generated the random token
+function createToken() {
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; 
+    $abbreviation = substr(str_shuffle($chars),0,5); 
+    return $abbreviation;
+}
+
+// The resolve if: takes the link, fragment it in 2 parts, check if the first part is the name of the server and the second one use it as a token to search in the database
+$urlCheck = substr($url, 0, 16);
+//Now I will check if the first 16 characters are the name of my server
+if ($urlCheck == 'http://localhost') { 
+    $length = strlen($url);
+    $checked = substr($url, 17 , $length);
+    //get the token from the URL
+    $stmt = $db->prepare('SELECT link FROM links WHERE token = ?'); 
+    $stmt->bind_param("s", $checked); 
+    $stmt->execute(); 
+    $stmt->store_result();
+    if ($stmt->num_rows == 1) { 
+        $stmt->bind_result($url); 
+        $stmt->fetch(); 
+        echo $url; 
+    }
+
+} else if($url) {
     $stmt = $db->prepare('SELECT token FROM links WHERE link= ?');
     $stmt->bind_param("s", $url); //give $url to the query
     $stmt->execute(); //run query
@@ -22,8 +46,7 @@ if($url) {
 
         //If it doesn't exist, generate a token
     } else {
-        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; 
-        $token = substr(str_shuffle($chars),0,5);
+        $token = createToken();
         $stmt = $db->prepare("INSERT INTO links VALUES (NULL, ?, ?, NOW(), 0");
         $stmt->bind_param('ss', $url, $token);  // s is the type of the variable introduced in the query, this time string
 
